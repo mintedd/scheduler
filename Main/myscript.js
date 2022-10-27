7767216c3d38669aae32c1e6ca253847cdef56b6
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-10-27 12:45:44 -0400

    corrected time in header

--- Main/myscript.js
+++ Main/myscript.js
@@ -1,5 +1,5 @@
 //add "todays date" in header
-var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentDate = moment().format('dddd MMMM Do YYYY, h:mm a');
 $('#currentDay').text(currentDate)
 
 //need the gray, red, and green
@@ -8,5 +8,12 @@ $('#currentDay').text(currentDate)
 //green is in the future
 
 //localstorage
+//going to occur on a click
+var saveBtn = $('.saveBtn')
+
+saveBtn.on('click', function(){
+    //textarea content is going to be saved 
+
+})
 
 //preventDefault
