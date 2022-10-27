1f7c2868f54e58de77c07feea2f412835828e57a
mintedd <xxxxiheartyouxxxxx@gmail.com>
2022-10-27 12:39:36 -0400

    added date in header

--- Main/myscript.js
+++ Main/myscript.js
@@ -1,4 +1,6 @@
 //add "todays date" in header
+var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+$('#currentDay').text(currentDate)
 
 //need the gray, red, and green
 //gray represents time past
